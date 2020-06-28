8e9b64931c60be7ec950dcb183a66bba15919487
Phi Tien Hoang <67533299+shinhoang88@users.noreply.github.com>
2020-06-28 20:12:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description="A OpenAI Gym Env for Robotory downscale manipulator",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    # url="https://github.com/shinhoang88/gym-robotorydownscale",
+    url="https://github.com/shinhoang88/gym-robotorydownscale",
     packages=setuptools.find_packages(include="gym_robotorydownscale*"),
     install_requires=['gym', 'pybullet', 'numpy']  # And any other dependencies foo needs
 )
